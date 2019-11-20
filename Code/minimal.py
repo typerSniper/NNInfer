1239598300f1c6a5cd7a7aa1737ff0f0a19e8fd4
Jatin Arora <jatina29@gmail.com>
2019-11-20 16:53:20 -0500

    find minimal code added

--- Code/minimal.py
+++ Code/minimal.py
@@ -5,11 +5,11 @@ import numpy as np
 def findMinimal (activation_pattern, properties):
 	assert(isSatisfied(activation_pattern, properties))
 
-	net_dim = activation_pattern.shape
+	num_layers = activation_pattern.shape
 
 	m_activation_pattern = np.copy(activation_pattern)
 
-	for layer in range(net_dim[0]-1, -1, -1):
+	for layer in range(num_layers[0]-1, -1, -1):
 		
 		num_neurons = (m_activation_pattern[layer].shape)[0]
 		m_activation_pattern[layer] = np.repeat(2, num_neurons)
@@ -34,16 +34,15 @@ def findMinimalInLayer(activation_pattern, properties, layerNum):
 	while len(candidates)!=0:
 
 		candidate = getNext(candidates, activation_pattern, properties, layerNum)
+		np.delete(candidates, candidate)
 		initial_status = m_activation_pattern[layer][candidate]
-		
 		m_activation_pattern[layer][candidate] = 2
-		
+
 		if initial_status==2 or isSatisfied(m_activation_pattern, properties):
-			np.delete(candidates, candidate)
 			continue
 		else:
 			m_activation_pattern[layer][candidate] = initial_status
-			return m_activation_pattern
+	return m_activation_pattern			
 
 
 
