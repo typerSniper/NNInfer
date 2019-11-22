b7b11bc0a6a619cbcc2b43cca8d4a8a3bf38a57b
saketdingliwal <saketdingliwal97@gmail.com>
2019-11-22 15:15:36 -0500

    cleaned?

--- Code/main.py
+++ Code/main.py
@@ -6,7 +6,7 @@ from minimal import findMinimal
 def main():
 	num_neurons = [2,3,3,2]
 	model = Model(num_neurons)
-	nnet_name = 'dummy.nnet'
+	nnet_name = 'scratch/dummy.nnet'
 	model.write_NNET(nnet_name)
 	for r in range(10):
 		print("On random input " + str(r))
